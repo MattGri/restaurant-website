a16af4f5936e6e6f24b05892afad53874294b4eb
Mateusz <mateusz.grzybowski98@gmail.com>
2022-11-12 12:00:00 +0100

    Started creating navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+const Navigation = () => {
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
