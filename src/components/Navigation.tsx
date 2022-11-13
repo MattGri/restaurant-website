8679e902b64c7e15f676096fe1bc2c680b16b590
Mateusz <mateusz.grzybowski98@gmail.com>
2022-11-13 17:05:30 +0100

    added nav items and scss

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import '../styles/navigation.scss';
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className="navigation">
+      <div></div>
+      <ul className="links">
+        <li className='linkItems'>
+          <a href='#home'>Home</a>
+          <a href='#about'>About</a>
+          <a href='#menu'>Menu</a>
+          <a href='#awards'>Awards</a>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
